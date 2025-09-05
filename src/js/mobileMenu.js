2a8a8fc7b3d61eb6bde992c03d93053a81ca9de2
Riky-Droyg <riky.droyg@gmail.com>
2025-09-05 11:26:33 +0300

    update link und mobile menu 1.0

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -8,7 +8,18 @@
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
+  // ⬇️ закриваємо меню при кліку на будь-який <a> всередині [data-menu]
+  refs.menu.addEventListener('click', (e) => {
+    const link = e.target.closest('a[href]');
+    if (!link) return;
+    closeMenu();
+  });
+
   function toggleMenu() {
     refs.menu.classList.toggle('is-open');
+  }
+
+  function closeMenu() {
+    refs.menu.classList.remove('is-open');
   }
 })();
